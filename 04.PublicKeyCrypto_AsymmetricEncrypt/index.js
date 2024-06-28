872eeb9a80eabd61169d880838d05a5592f52d3f
Ahindra D <official.ahindra@gmail.com>
2024-06-29 04:27:34 +0530

    refactor

--- 04.PublicKeyCrypto_AsymmetricEncrypt/index.js
+++ 04.PublicKeyCrypto_AsymmetricEncrypt/index.js
@@ -1,6 +1,7 @@
 import { Keypair } from "@solana/web3.js";
 import nacl from "tweetnacl";
-import { decodeUTF8 } from "tweetnacl-util";
+import pkg from 'tweetnacl-util';
+const { decodeUTF8 } = pkg;
 
 
 const key_pair=Keypair.generate();
@@ -14,3 +15,15 @@ console.log('signature: ', signature);
 const verified=nacl.sign.detached.verify(message_bytes,signature,key_pair.publicKey);
 //verifying with public Key
 console.log('verified: ', verified);
+
+
+//also messages encrypted with public key can be decrypted with private key
+//encrypted with public key
+const message2 = "this is another secret message";
+const message_bytes2 = decodeUTF8(message2);
+const encrypted2=nacl.box(message_bytes2,key_pair.publicKey,key_pair.secretKey);
+console.log('encrypted: ', encrypted);
+
+//decrypted with private key    
+const decrypted=nacl.box.open(encrypted2,key_pair.secretKey,key_pair.publicKey);
+console.log('decrypted: ', decrypted);
